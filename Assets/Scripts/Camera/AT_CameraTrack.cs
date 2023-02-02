f638d4326a122ee59c81f2a6eaa4db2cec51353e
jkcadee <jkwok55@my.bcit.ca>
2023-02-01 22:06:07 -0800

    fixed file structure

---
+++ Assets/Scripts/Camera/AT_CameraTrack.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AT_CameraTrack : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        transform.LookAt(target.transform);
+    }
+}
