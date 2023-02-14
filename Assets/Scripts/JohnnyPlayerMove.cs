29059a8564e2785b1f0177165f687d7bd6251d10
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2023-02-13 23:49:40 -0800

    sound + smoother walking
    => sound from momo's branch
    => made walking smoother

--- Assets/Scripts/JohnnyPlayerMove.cs
+++ Assets/Scripts/JohnnyPlayerMove.cs
@@ -40,24 +40,18 @@ public class JohnnyPlayerMove : MonoBehaviour
 
         if(mov == Vector2.zero) {
             anim.SetBool("isWalking", false);
-            Debug.Log("bruh");
-            if (am.sounds["Walk"].isPlaying)
-                am.sounds["Walk"].Pause();
         } else {
             anim.SetBool("isWalking", true);
-            Debug.Log("bruh1231234");
-            if (!am.sounds["Walk"].isPlaying)
-                am.sounds["Walk"].Play();
         }
 
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + (mov.x * rotationSpeed), transform.localEulerAngles.z);
         rb.velocity = Vector3.RotateTowards(rb.velocity, transform.forward, 4f, 0);
 
-        rb.AddForce(transform.forward * mov.y*5, ForceMode.VelocityChange);
+        rb.AddForce(transform.forward * mov.y * accelerationSpeed, ForceMode.VelocityChange);
         if (rb.velocity.magnitude > speedLimit)
             rb.velocity = rb.velocity.normalized * speedLimit;
         
-        if (rb.velocity.magnitude < 1.0f)
+        if (rb.velocity.magnitude < 0.3f)
         {
             if (am.sounds["Walk"].isPlaying)
                 am.sounds["Walk"].Pause();
