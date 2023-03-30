add45c629a9836b0f2939476f38060fa6c1b21a8
Amber Ma <agmlammy@gmail.com>
2023-03-30 09:56:10 -0700

    I've added the inventory section of the game (please note that interactable scripts must call the collectData method at least once and contain a journalentry object (with a name and description)

--- Assets/Scripts/Player_Data/PlayerData.cs
+++ Assets/Scripts/Player_Data/PlayerData.cs
@@ -80,10 +80,15 @@ public class PlayerData : MonoBehaviour
         return text_color;
     }
 
+    public static List<JournalEntry> getJournalList() {
+        return journal_list;
+    }
+
     public static void reset() {
         default_color = Color.white;
         text_color = default_color;
         volume_percent = 100;
         journal_list = new List<JournalEntry>();
+        map_list = new List<MapObject>();
     }
 }
